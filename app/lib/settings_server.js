736163092d23da9c85248ae7f5ab1714f2c0fe01
gkobeaga <gkobeaga@riseup.net>
2015-12-03 12:39:00 +0100

    changed the incidents update interval (5h/5m)

--- app/lib/settings_server.js
+++ app/lib/settings_server.js
@@ -1,9 +1,7 @@
 if (Meteor.isServer) {
 
 
- // s.rt_incidents_update_interval = 1000 * 60 * 5;
-  s.rt_incidents_update_interval = 1000 * 30;
-  //s.hist_incidents_update_interval = 1000 * 60;
-  s.hist_incidents_update_interval = 1000 * 60 * 3;
+  s.rt_incidents_update_interval = 1000 * 60 * 5;
+  s.hist_incidents_update_interval = 1000 * 60 * 5;
 
 }
