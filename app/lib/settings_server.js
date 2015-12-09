d263572a94225c3c19e66180c824cb5c53ae0c9e
gkobeaga <gkobeaga@riseup.net>
2015-12-10 00:56:42 +0100

    Update rate of incidents history changed to 5h

--- app/lib/settings_server.js
+++ app/lib/settings_server.js
@@ -2,6 +2,6 @@ if (Meteor.isServer) {
 
 
   s.rt_incidents_update_interval = 1000 * 60 * 5;
-  s.hist_incidents_update_interval = 1000 * 60 * 5;
+  s.hist_incidents_update_interval = 1000 * 60 * 60 * 5;
 
 }
